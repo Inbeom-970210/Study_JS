e0231a2f3e9af1cbd999f7dfdeaa754b8c2df990
Inbeom Kim <dlsqja970210@naver.com>
2023-06-28 21:04:34 +0900

    Study variables

--- #2 WELCOME TO JAVASCIPT/momentum/app.js
+++ #2 WELCOME TO JAVASCIPT/momentum/app.js
@@ -1,1 +1,22 @@
-alert("hi");
+const a = 5;
+const b = 2;    // 변하지 않는 변수
+var veryLongVariableName = 0;   // 예전 변수. let이랑 비슷 (쓰지말자!)
+let myName = "inbeom";  // 변할 수 있는 변수
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "InbeomKim";
+
+console.log("your new name is  " + myName);
+
+//////////////////////////////////////////////
+///////////////////////////////////////////////
+// 아무것도 없음을 의미 (false랑 의미가 다름). 메모리에 존재하고, 값이 주어졌는데 그값은 비어있다
+const amIFat = null;    
+let something;
+console.log(amIFat);
+// undefined 출력. 메모리에 존재하는데 값이 없는 상태
+console.log(something);
